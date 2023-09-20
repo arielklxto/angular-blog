8b9ba66516caa97b95f0eaaac8cb3f3c1f9a0b75
ariel <arieljond@gmail.com>
2023-09-20 19:10:04 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake =  [
+    {
+        "id":"1",
+        "title": "NOVIDADES NO ANGULAR!",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea similique, ducimus in, pariatur, molestias et ad nulla explicabo commodi aliquam tempora voluptatibus non dolor officia quibusdam dolores voluptas eligendi cumque!",
+        "photoCover": "https://i.stack.imgur.com/luaq2.png"
+    },
+    {
+        "id":"2",
+        "title": "Você sabe codar com Python?",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea similique, ducimus in, pariatur, molestias et ad nulla explicabo commodi aliquam tempora voluptatibus non dolor officia quibusdam dolores voluptas eligendi cumque!",
+        "photoCover": "https://itforum.com.br/wp-content/uploads/2020/10/linguagem-programacao-python-1.jpg"
+    },
+    {
+        "id":"3",
+        "title": "Qual é a sua linguagem de programação favorita?",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea similique, ducimus in, pariatur, molestias et ad nulla explicabo commodi aliquam tempora voluptatibus non dolor officia quibusdam dolores voluptas eligendi cumque!",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS564hHpF1pImuLdu5jdCCPIgjayniRvr-D5gzXnyPtFGQ69qIxjuVAz2yNfqocSSIQWHc&usqp=CAU"
+    },
+    {
+        "id":"4",
+        "title": "JavaScript, Java ou C++?",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea similique, ducimus in, pariatur, molestias et ad nulla explicabo commodi aliquam tempora voluptatibus non dolor officia quibusdam dolores voluptas eligendi cumque!",
+        "photoCover": "https://www.nexojornal.com.br/incoming/imagens/Veja-em-gr%C3%A1ficos-A-idade-e-a-popularidade-das-linguagens-de-programa%C3%A7%C3%A3o/alternates/LANDSCAPE_768/Veja%20em%20gr%C3%A1ficos%3A%20A%20idade%20e%20a%20popularidade%20das%20linguagens%20de%20programa%C3%A7%C3%A3o"
+    }
+]
